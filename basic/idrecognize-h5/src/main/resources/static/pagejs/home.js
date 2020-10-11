e234875db05c2dd4fe61e5fe30cd7d456fbba480
jushi.zf <jushi.zf@alipay.com>
2020-10-11 19:25:37 +0800

    prepend license in each static file of front-end

--- basic/idrecognize-h5/src/main/resources/static/pagejs/home.js
+++ basic/idrecognize-h5/src/main/resources/static/pagejs/home.js
@@ -1,100 +1,121 @@
+/*
+ * Copyright (c) 2020 ZOLOZ PTE.LTD.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-  var elements = document.getElementsByClassName('routerView');
-  var zListContentEl = document.getElementById('zListContent');
-  var all_options = zListContentEl.options;
-  var clientDocType = '';
-  var zListLabelEl = document.getElementById('zListLabel');
-  var zHelperMsgEl = document.getElementById('zHelperMsg');
-  var docSelectorModel = {
-    submit_key: 'clientDocType',
-    content: '',
-    optional_label: '',
-    error_msg: 'Field can\'t be blank',
-    options: [
-      {key: '00000001003', value: 'Passport'},
-      {key: '08520000001', value: 'HKID1'},
-      {key: '08520000002', value: 'HKID2'},
-      {key: '00630000001', value: 'UMID'},
-      {key: '00630000002', value: 'TIN ID'},
-      {key: '00630000024', value: 'Philhealth Card'},
-      {key: '00630000004', value: 'Driver\'s License'},
-      {key: '00630000020', value: 'SSS ID'},
-      {key: '00630000022', value: 'Voter\'s ID'},
-      {key: '00630000017', value: 'PRC ID'},
-      {key: '00630000029', value: 'POB'},
-      {key: '00000001004', value: 'New Passport(Two Pages)'},
-    ]
-  };
-  window.onload = () => {
-    for (const element of elements) {
-      element.style.minHeight = window.innerHeight + 'px';
-    }
-    zListContentEl.children[0].style.display = 'none';
-    docSelectorModel.options.forEach(item => {
-      const newOption = document.createElement('option');
-      newOption.setAttribute('value', item.key);
-      newOption.innerHTML = item.value;
-      zListContentEl.appendChild(newOption);
-    })
-    if(window.localStorage.getItem(docSelectorModel.submit_key)) {
-      for(var i = 0; i<all_options.length; i++) {
-        if(all_options[i].value === window.localStorage.getItem(docSelectorModel.submit_key)) {
-          all_options[i].selected = true;
-          zListLabelEl.style.top = 0;
-          zListLabelEl.style.fontSize = '0.9rem';
-        }
+var elements = document.getElementsByClassName('routerView');
+var zListContentEl = document.getElementById('zListContent');
+var all_options = zListContentEl.options;
+var clientDocType = '';
+var zListLabelEl = document.getElementById('zListLabel');
+var zHelperMsgEl = document.getElementById('zHelperMsg');
+var docSelectorModel = {
+  submit_key: 'clientDocType',
+  content: '',
+  optional_label: '',
+  error_msg: 'Field can\'t be blank',
+  options: [
+    {key: '00000001003', value: 'Passport'},
+    {key: '08520000001', value: 'HKID1'},
+    {key: '08520000002', value: 'HKID2'},
+    {key: '00630000001', value: 'UMID'},
+    {key: '00630000002', value: 'TIN ID'},
+    {key: '00630000024', value: 'Philhealth Card'},
+    {key: '00630000004', value: 'Driver\'s License'},
+    {key: '00630000020', value: 'SSS ID'},
+    {key: '00630000022', value: 'Voter\'s ID'},
+    {key: '00630000017', value: 'PRC ID'},
+    {key: '00630000029', value: 'POB'},
+    {key: '00000001004', value: 'New Passport(Two Pages)'},
+  ]
+};
+window.onload = () => {
+  for (const element of elements) {
+    element.style.minHeight = window.innerHeight + 'px';
+  }
+  zListContentEl.children[0].style.display = 'none';
+  docSelectorModel.options.forEach(item => {
+    const newOption = document.createElement('option');
+    newOption.setAttribute('value', item.key);
+    newOption.innerHTML = item.value;
+    zListContentEl.appendChild(newOption);
+  })
+  if(window.localStorage.getItem(docSelectorModel.submit_key)) {
+    for(var i = 0; i<all_options.length; i++) {
+      if(all_options[i].value === window.localStorage.getItem(docSelectorModel.submit_key)) {
+        all_options[i].selected = true;
+        zListLabelEl.style.top = 0;
+        zListLabelEl.style.fontSize = '0.9rem';
       }
     }
-  clientDocType = zListContentEl.options[zListContentEl.selectedIndex].value;
   }
-  onchange = (e) => {
-    zListLabelEl.style.top = 0;
-    zListLabelEl.style.fontSize = '0.9rem';
-    clientDocType = e.target.value;
-    window.localStorage.setItem(docSelectorModel.submit_key, e.target.value);
-  }
-  onFocus = () => {
-      zListLabelEl.style.color = '#787878';
-      zListContentEl.style.borderColor = '#aaa';
-      zHelperMsgEl.innerHTML = '';
-  }
+clientDocType = zListContentEl.options[zListContentEl.selectedIndex].value;
+}
+onchange = (e) => {
+  zListLabelEl.style.top = 0;
+  zListLabelEl.style.fontSize = '0.9rem';
+  clientDocType = e.target.value;
+  window.localStorage.setItem(docSelectorModel.submit_key, e.target.value);
+}
+onFocus = () => {
+    zListLabelEl.style.color = '#787878';
+    zListContentEl.style.borderColor = '#aaa';
+    zHelperMsgEl.innerHTML = '';
+}
 
-  initDocPro = async (data) => {
-    const url = '/api/idrecognition/initialize';
-    const options = {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-    return request(url, options);
-  }
+initDocPro = async (data) => {
+  const url = '/api/idrecognition/initialize';
+  const options = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  return request(url, options);
+}
 
-  takePhoto = async () => {
-    if(clientDocType === '') {
-      zHelperMsgEl.style.color = '#FF3342';
-      zListLabelEl.style.color = '#FF3342';
-      zListContentEl.style.borderColor = '#FF3342';
-      zHelperMsgEl.innerHTML = docSelectorModel.error_msg;
-      return;
-    }
-    const initData = {
-      docType: clientDocType,
-    };
-    const response = await initDocPro(initData);
-    if (response.error === 'NETWORK_ERROR') {
-      alert('Network Error');
-    } else if (response.result && response.result.resultCode === 'SUCCESS') {
-      const idrecognizeUrl = 'https://sg-production-cdn.zoloz.com/page/zoloz-doc-fe/index.html';
-      const clientCfg = response.clientCfg;
-      const state = response.transactionId;
-      const href = window.location.href;
-      const baseUrl = href.split('#')[0];
-      const callbackUrl = baseUrl + '/result.html';
-      window.location.href = `${idrecognizeUrl}?state=${state}&clientcfg=${encodeURIComponent(clientCfg)}&callbackurl=${encodeURIComponent(callbackUrl)}`;
-    } else {
-      alert('System Error');
-    }
+takePhoto = async () => {
+  if(clientDocType === '') {
+    zHelperMsgEl.style.color = '#FF3342';
+    zListLabelEl.style.color = '#FF3342';
+    zListContentEl.style.borderColor = '#FF3342';
+    zHelperMsgEl.innerHTML = docSelectorModel.error_msg;
+    return;
   }
+  const initData = {
+    docType: clientDocType,
+  };
+  const response = await initDocPro(initData);
+  if (response.error === 'NETWORK_ERROR') {
+    alert('Network Error');
+  } else if (response.result && response.result.resultCode === 'SUCCESS') {
+    const idrecognizeUrl = 'https://sg-production-cdn.zoloz.com/page/zoloz-doc-fe/index.html';
+    const clientCfg = response.clientCfg;
+    const state = response.transactionId;
+    const href = window.location.href;
+    const baseUrl = href.split('#')[0];
+    const callbackUrl = baseUrl + '/result.html';
+    window.location.href = `${idrecognizeUrl}?state=${state}&clientcfg=${encodeURIComponent(clientCfg)}&callbackurl=${encodeURIComponent(callbackUrl)}`;
+  } else {
+    alert('System Error');
+  }
+}
--- basic/realid-h5/src/main/resources/static/pagejs/result.js
+++ basic/realid-h5/src/main/resources/static/pagejs/result.js
@@ -20,77 +20,77 @@
  * SOFTWARE.
  */
 
-  var elements = document.getElementsByClassName('routerView');
-  var docImageEl = document.getElementById('docImage');
-  var faceImageEl = document.getElementById('faceImage');
-  var ekycResultEl = document.getElementById('ekycResult');
-  var scoreEl = document.getElementById('score');
-  var riskEl = document.getElementById('risk');
-  var interruptEl = document.getElementById('interrupt');
-  var resultMainEl = document.getElementById('resultMain');
-  var docImage = '';
-  var faceImage = '';
-  var ekycResult = '';
-  var score = '';
-  var risk = '';
-  window.onload = () => {
-    this.created();
-    for (const element of elements) {
-      element.style.minHeight = window.innerHeight + 'px';
-    }
+var elements = document.getElementsByClassName('routerView');
+var docImageEl = document.getElementById('docImage');
+var faceImageEl = document.getElementById('faceImage');
+var ekycResultEl = document.getElementById('ekycResult');
+var scoreEl = document.getElementById('score');
+var riskEl = document.getElementById('risk');
+var interruptEl = document.getElementById('interrupt');
+var resultMainEl = document.getElementById('resultMain');
+var docImage = '';
+var faceImage = '';
+var ekycResult = '';
+var score = '';
+var risk = '';
+window.onload = () => {
+  this.created();
+  for (const element of elements) {
+    element.style.minHeight = window.innerHeight + 'px';
   }
+}
 
-  
-  checkResult = async (data) => {
-    const url = baseUrl + '/api/checkresult';
-    const options = {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-    return request(url, options);
+
+checkResult = async (data) => {
+  const url = baseUrl + '/api/checkresult';
+  const options = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  return request(url, options);
+}
+
+created = async () => {
+  const response = JSON.parse(decodeURIComponent(getUrlParam('response')));
+  const state = response.state;
+  if (response.code === 1003) {
+    interruptEl.style.display = 'block';
+    resultMainEl.style.display = 'none';
+    return;
   }
-
-  created = async () => {
-    const response = JSON.parse(decodeURIComponent(getUrlParam('response')));
-    const state = response.state;
-    if (response.code === 1003) {
-      interruptEl.style.display = 'block';
-      resultMainEl.style.display = 'none';
-      return;
+  const checkData = {
+    transactionId: state,
+    isReturnImage: 'Y'
+  }
+  const result = await checkResult(checkData);
+  if (result.retCode === 404) {
+    alert('Network Error');
+  } else {
+    if (result.extIdInfo && result.extIdInfo.frontPageImg) {
+      docImage = `data:image/png;base64,${result.extIdInfo.frontPageImg}`;
     }
-    const checkData = {
-      transactionId: state,
-      isReturnImage: 'Y'
+    if (result.extFaceInfo && result.extFaceInfo.faceImg) {
+      faceImage = `data:image/png;base64,${result.extFaceInfo.faceImg}`;
     }
-    const result = await checkResult(checkData);
-    if (result.retCode === 404) {
-      alert('Network Error');
+    ekycResult = result.ekycResult || '-';
+    score = (result.extFaceInfo && result.extFaceInfo.faceScore.toFixed(2)) || '-';
+    if (result.extRiskInfo && result.extRiskInfo.ekycResultRisk) {
+      risk = result.extRiskInfo.ekycResultRisk;
     } else {
-      if (result.extIdInfo && result.extIdInfo.frontPageImg) {
-        docImage = `data:image/png;base64,${result.extIdInfo.frontPageImg}`;
-      }
-      if (result.extFaceInfo && result.extFaceInfo.faceImg) {
-        faceImage = `data:image/png;base64,${result.extFaceInfo.faceImg}`;
-      }
-      ekycResult = result.ekycResult || '-';
-      score = (result.extFaceInfo && result.extFaceInfo.faceScore.toFixed(2)) || '-';
-      if (result.extRiskInfo && result.extRiskInfo.ekycResultRisk) {
-        risk = result.extRiskInfo.ekycResultRisk;
-      } else {
-        risk = '-';
-      }
-      ekycResultEl.innerHTML = this.ekycResult;
-      scoreEl.innerHTML = this.score;
-      riskEl.innerHTML = this.risk;
-      docImageEl.setAttribute('src', this.docImage)
-      faceImageEl.setAttribute('src', this.faceImage)
+      risk = '-';
     }
+    ekycResultEl.innerHTML = this.ekycResult;
+    scoreEl.innerHTML = this.score;
+    riskEl.innerHTML = this.risk;
+    docImageEl.setAttribute('src', this.docImage)
+    faceImageEl.setAttribute('src', this.faceImage)
   }
+}
 
-  okClick = () => {
-    window.history.go(-1);
-  }
+okClick = () => {
+  window.history.go(-1);
+}
